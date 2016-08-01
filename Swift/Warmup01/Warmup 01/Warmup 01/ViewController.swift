6e289bb46a086251bcff2f2228ea2e7302f42025
Pedro Prevost <p.prevost@me.com>
2016-08-01 20:53:52 +0200

    Toevoegen Warmup01e

---
+++ Swift/Warmup01/Warmup 01/Warmup 01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warmup 01
+//
+//  Created by Pedro Prevost on 01/08/2016.
+//  Copyright © 2016 Pedro Prevost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swift/Warmup01/Warmup 01/Warmup 01Tests/Warmup_01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Warmup_01Tests.swift
+//  Warmup 01Tests
+//
+//  Created by Pedro Prevost on 01/08/2016.
+//  Copyright © 2016 Pedro Prevost. All rights reserved.
+//
+
+import XCTest
+@testable import Warmup_01
+
+class Warmup_01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
