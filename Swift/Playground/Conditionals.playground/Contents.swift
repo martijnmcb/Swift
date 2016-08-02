84246605abdce84d74d8d98fec71c3153f2c8b0e
Pedro Prevost <p.prevost@me.com>
2016-08-02 22:08:32 +0200

    Updated Playground

--- Swift/Playground/Conditionals.playground/Contents.swift
+++ Swift/Playground/Conditionals.playground/Contents.swift
@@ -1,13 +1,36 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
+/*
 var isMyHousOnFire: Bool = true
+var isBluswaterop: Bool = true
 
-
-if isMyHousOnFire {
-    print ("FIK", terminator:"  ")
+if isMyHousOnFire && isBluswaterop{
+    print ("Niets meer aan te doen", terminator:"  ")
 } else
-    {print ("geen fik", terminator:"")
+    {print ("Geen probleem", terminator:"")
+    
+
+
+
+func test() {
+print("this is my first function!")
+}
+
+test()
+
+ */
+
+var getala = 10.0
+var getalb = 200.0
+
+
+
+func optellen(getal1: Double, getal2: Double)->Double {
+ 
+ 
+  return getal1 + getal2
     
 }
+
+var saldo = optellen(getala, getal2:getalb)
